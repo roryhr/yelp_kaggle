40152e1d42bf7ab1121ffba57278e3970059575f
rory <roryhr@gmail.com>
2016-03-12 14:37:55 -0800

    using graph_factory

--- resnet_graph.py
+++ resnet_graph.py
@@ -12,10 +12,10 @@ from keras.layers.normalization import BatchNormalization
 from keras.models import Graph
 from keras.layers.core import Dense, Activation, Flatten, Reshape
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.convolutional import AveragePooling2D, ZeroPadding2D
+from keras.layers.convolutional import AveragePooling2D
 from keras.optimizers import SGD
 
-from helper_functions import load_and_preprocess, mean_f1_score, resnet_image_processing
+from helper_functions import mean_f1_score, resnet_image_processing
 from helper_functions import show_image_labels
 
 from joblib import Parallel, delayed
@@ -24,7 +24,7 @@ import tables
 
 #%% Configuration 
 number_of_epochs = 15
-n_images = 1000
+n_images = 2000
 imsize   = 224  # Square images
 save_model = False
 show_plots = True
@@ -132,109 +132,187 @@ train_ind, test_ind, _, _ = train_test_split(range(n_images),
                                              random_state=4)
 
 print 'Mean for all images: {}'.format(im_mean)
+#%%
+
+def building_block(graph, layer_nb, conv_nb, input_name, nb_filters):
+    """ Add a Residual building block
+    
+
+    """
+#    First convolution 
+    first_convolution = 'conv{}_{}'.format(layer_nb,conv_nb)
+    first_normalization = 'bn_{}_{}'.format(layer_nb,conv_nb)
+    first_activation = 'relu{}_{}'.format(layer_nb, conv_nb)
+
+#    Second convolution 
+    second_convolution = 'conv{}_{}'.format(layer_nb,conv_nb+1)
+    second_normalization = 'bn_{}_{}'.format(layer_nb,conv_nb+1)
+    second_activation = 'relu{}_{}'.format(layer_nb, conv_nb+1)
+    
+#    return [first_convolution, first_normalization, first_activation,
+#            second_convolution, second_normalization, second_activation]
+    
+    graph.add_node(Convolution2D(nb_filters, 3, 3, W_regularizer=l2(weight_decay), 
+                             border_mode='same'),
+               name=first_convolution, input=input_name)
+    graph.add_node(BatchNormalization(), name=first_normalization,
+                   input=first_convolution)
+    graph.add_node(Activation('relu'), name=first_activation,
+                   input=first_normalization)
+    
+#    Second Convolution
+    graph.add_node(Convolution2D(nb_filters, 3, 3, W_regularizer=l2(weight_decay), 
+                                 border_mode='same'),
+                   name=second_convolution, input=first_activation)
+    graph.add_node(BatchNormalization(), name=second_normalization,
+                   input=second_convolution)
+    graph.add_node(Activation('relu'), name=second_activation,
+                   inputs=[input_name,second_normalization], 
+                   merge_mode='sum')
+    
+    return graph, second_activation
+
+
 
 #%% ResNet-like 
-"""A scale model of the 34 layer ResNet with skip connections 
-
+"""18 layer ResNet with skip connections 
 
     http://arxiv.org/abs/1512.03385
 """
+nb_filters = 16
+
 graph = Graph()
 graph.add_input(name='input', input_shape=(3,imsize,imsize))   
-graph.add_node(Convolution2D(nb_filter=64, nb_row=7, nb_col=7,
+graph.add_node(Convolution2D(nb_filter=nb_filters, nb_row=7, nb_col=7,
                              input_shape=(3,imsize,imsize),
                              border_mode='same',
                              subsample=(2,2),
                              dim_ordering='th',
                              W_regularizer=l2(weight_decay)), 
                              name='conv1', input='input')
+# Output shape = (None,16,112,112)
 graph.add_node(BatchNormalization(), name='bn1', input='conv1')
 graph.add_node(Activation('relu'), name='relu1', input='bn1')
 graph.add_node(MaxPooling2D(pool_size=(3,3), strides=(2,2),
                             border_mode='same'), 
                name='pool1', input='relu1')
-# Output shape = (56,56)x64
-               
-graph.add_node(Convolution2D(64, 3, 3, W_regularizer=l2(weight_decay), 
-                             border_mode='same'),
-               name='conv2_1', input='pool1')
-graph.add_node(BatchNormalization(), name='bn2_1', input='conv2_1')
-graph.add_node(Activation('relu'), name='relu2_1', input='bn2_1')
-graph.add_node(Convolution2D(64, 3, 3, W_regularizer=l2(weight_decay), 
-                             border_mode='same'),
-               name='conv2_2', input='relu2_1')
-graph.add_node(BatchNormalization(), name='bn2_2', input='conv2_2')
-graph.add_node(Activation('relu'), name='relu2_2', inputs=['pool1','bn2_2'], 
-               merge_mode='sum')
-# Output shape = (64,56,56)
+# Output shape = (None,32,56,56)
 
-graph.add_node(Convolution2D(128, 3, 3, W_regularizer=l2(weight_decay), 
+#-------------------------- Layer Group 2 -------------------------------------
+graph, output_name = building_block(graph, layer_nb=2, conv_nb=1,
+                                    input_name='pool1', nb_filters=nb_filters)
+graph, output_name = building_block(graph, layer_nb=2, conv_nb=3,
+                                    input_name=output_name, nb_filters=nb_filters)
+graph, output_name = building_block(graph, layer_nb=2, conv_nb=5,
+                                    input_name=output_name, nb_filters=nb_filters)
+# Output shape = (None,16,56,56)
+
+#-------------------------- Layer Group 3 -------------------------------------
+graph.add_node(Convolution2D(nb_filters*2, 3, 3, W_regularizer=l2(weight_decay), 
                              border_mode='same', subsample=(2,2)),
-               name='conv3_1', input='relu2_2')
+               name='conv3_1', input=output_name)
+# Output shape = (None,32,28,28)
 graph.add_node(BatchNormalization(), name='bn3_1', input='conv3_1')
 graph.add_node(Activation('relu'), name='relu3_1', input='bn3_1')
-graph.add_node(Convolution2D(128, 3, 3, W_regularizer=l2(weight_decay), 
+graph.add_node(Convolution2D(nb_filters*2, 3, 3, W_regularizer=l2(weight_decay), 
                              border_mode='same'),
                name='conv3_2', input='relu3_1')
 graph.add_node(BatchNormalization(), name='bn3_2', input='conv3_2')
-# Output shape = (128,28,28)
 
-graph.add_node(Convolution2D(32,1,1, W_regularizer=l2(weight_decay)),
+graph.add_node(Convolution2D(nb_filters/2,1,1, W_regularizer=l2(weight_decay)),
                name='short3_1', input='relu2_2')
 graph.add_node(BatchNormalization(), name='short3_2', input='short3_1')
 # Output tensor shape = (32,56,56)
-graph.add_node(Reshape((128,28,28)), name='short3_3', input='short3_2')               
+graph.add_node(Reshape((nb_filters*2,28,28)), name='short3_3', input='short3_2')               
 graph.add_node(Activation('relu'), name='relu3_2', inputs=['short3_3','bn3_2'], 
                merge_mode='sum')
-# Output shape = (128,28,28)
+# Output shape = (None,64,28,28)
 
+#nb_filters *= 2
+graph, output_name = building_block(graph, layer_nb=3, conv_nb=3,
+                                    input_name='relu3_2', 
+                                    nb_filters=nb_filters*2)
+graph, output_name = building_block(graph, layer_nb=3, conv_nb=5,
+                                    input_name=output_name, 
+                                    nb_filters=nb_filters*2)
+graph, output_name = building_block(graph, layer_nb=3, conv_nb=7,
+                                    input_name=output_name, 
+                                    nb_filters=nb_filters*2)        
 
-graph.add_node(Convolution2D(256, 3, 3, W_regularizer=l2(weight_decay), 
+#-------------------------- Layer Group 4 -------------------------------------
+graph.add_node(Convolution2D(nb_filters*4, 3, 3, W_regularizer=l2(weight_decay), 
                              border_mode='same', subsample=(2,2)),
-               name='conv4_1', input='relu3_2')
+               name='conv4_1', input=output_name)
 graph.add_node(BatchNormalization(), name='bn4_1', input='conv4_1')
 graph.add_node(Activation('relu'), name='relu4_1', input='bn4_1')
-graph.add_node(Convolution2D(256, 3, 3, W_regularizer=l2(weight_decay), 
+graph.add_node(Convolution2D(nb_filters*4, 3, 3, W_regularizer=l2(weight_decay), 
                              border_mode='same'),
                name='conv4_2', input='relu4_1')
 graph.add_node(BatchNormalization(), name='bn4_2', input='conv4_2')
 # Output shape = (256,14,14)
 
-graph.add_node(Convolution2D(64,1,1, W_regularizer=l2(weight_decay)),
+graph.add_node(Convolution2D(nb_filters,1,1, W_regularizer=l2(weight_decay)),
                name='short4_1', input='relu3_2')
 graph.add_node(BatchNormalization(), name='short4_2', input='short4_1')
 # Output shape = (64,28,28)
-graph.add_node(Reshape((256,14,14)), name='short4_3', input='short4_2')               
+graph.add_node(Reshape((nb_filters*4,14,14)), name='short4_3', input='short4_2')               
 graph.add_node(Activation('relu'), name='relu4_2', inputs=['short4_3','bn4_2'], 
                merge_mode='sum')
 # Output shape = (256,14,14)
 
-graph.add_node(Convolution2D(512, 3, 3, W_regularizer=l2(weight_decay), 
+graph, output_name = building_block(graph, layer_nb=4, conv_nb=3,
+                                    input_name='relu4_2', 
+                                    nb_filters=nb_filters*4)
+graph, output_name = building_block(graph, layer_nb=4, conv_nb=5,
+                                    input_name=output_name, 
+                                    nb_filters=nb_filters*4)
+graph, output_name = building_block(graph, layer_nb=4, conv_nb=7,
+                                    input_name=output_name, 
+                                    nb_filters=nb_filters*4)        
+graph, output_name = building_block(graph, layer_nb=4, conv_nb=9,
+                                    input_name=output_name, 
+                                    nb_filters=nb_filters*4)   
+graph, output_name = building_block(graph, layer_nb=4, conv_nb=11,
+                                    input_name=output_name, 
+                                    nb_filters=nb_filters*4)   
+# Output shape = (256,14,14)
+
+
+#-------------------------- Layer Group 5 -------------------------------------
+graph.add_node(Convolution2D(nb_filters*8, 3, 3, W_regularizer=l2(weight_decay), 
                              border_mode='same', subsample=(2,2)),
-               name='conv5_1', input='relu4_2')
+               name='conv5_1', input=output_name)
 graph.add_node(BatchNormalization(), name='bn5_1', input='conv5_1')
 graph.add_node(Activation('relu'), name='relu5_1', input='bn5_1')
-graph.add_node(Convolution2D(512, 3, 3, W_regularizer=l2(weight_decay), 
+graph.add_node(Convolution2D(nb_filters*8, 3, 3, W_regularizer=l2(weight_decay), 
                              border_mode='same'),
                name='conv5_2', input='relu5_1')
 graph.add_node(BatchNormalization(), name='bn5_2', input='conv5_2')
 # Output shape = (256,14,14)
 
-graph.add_node(Convolution2D(128,1,1, W_regularizer=l2(weight_decay)),
+graph.add_node(Convolution2D(nb_filters*2,1,1, W_regularizer=l2(weight_decay)),
                name='short5_1', input='relu4_2')
 graph.add_node(BatchNormalization(), name='short5_2', input='short5_1')
 # Output shape = (64,28,28)
-graph.add_node(Reshape((256,7,7)), name='short5_3', input='short5_2')               
+graph.add_node(Reshape((nb_filters*8,7,7)), name='short5_3', input='short5_2')               
 graph.add_node(Activation('relu'), name='relu5_2', inputs=['short5_3','bn5_2'], 
                merge_mode='sum')
 # Output shape = (512,7,7)
+               
+graph, output_name = building_block(graph, layer_nb=5, conv_nb=3,
+                                    input_name='relu5_2', 
+                                    nb_filters=nb_filters*8)
+graph, output_name = building_block(graph, layer_nb=5, conv_nb=5,
+                                    input_name=output_name, 
+                                    nb_filters=nb_filters*8)
+# Output shape = (128,7,7)
                       
 graph.add_node(AveragePooling2D(pool_size=(3,3), strides=(2,2),
                                 border_mode='same'),
-               name='pool2', input='relu5_2')               
+               name='pool2', input=output_name)               
 graph.add_node(Flatten(), name='flatten', input='pool2')
-graph.add_node(Dense(9, activation='sigmoid'), name='dense5', input='flatten')
-graph.add_output(name='output', input='dense5')
+graph.add_node(Dense(9, activation='sigmoid'), name='dense', input='flatten')
+graph.add_output(name='output', input='dense')
                
                
 sgd = SGD(lr=0.1, decay=1e-2, momentum=0.9)
@@ -248,7 +326,7 @@ graph.fit({'input': tensor[train_ind],
           validation_data={'input': tensor[test_ind],
                             'output': train_df.iloc[test_ind,label_start:].values},
           shuffle=True,
-          callbacks=[EarlyStopping(monitor='val_loss', patience=0, mode='min')],
+#          callbacks=[EarlyStopping(monitor='loss', patience=0, mode='min')],
           verbose=1)
 
 #%% Compute mean_f1_score
